7e0ee3052d06e4d51a302ef02a1695e6db5b167d
Matheus Nunes <matheus.nunes00@souunit.com.br>
2019-11-20 23:12:22 -0300

    Corrigindo requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import pandas as pd
 from geradorDeMatriz import geradorDeMatriz
 
-dataFilme = pd.read_csv("ml-latest-small\\movies.csv") 
-dataNotas = pd.read_csv("ml-latest-small\\ratings.csv")
-geradorDeMatriz = geradorDeMatriz(dataNotas,dataFilme)
-#matrizDeAdjacencia = geradorDeMatriz.gerarMatrizDeAdjacencia()
-matrizDeAdjacencia = geradorDeMatriz.biparteMatrix(dataFilme,dataNotas)
-
-#print(matrizDeAdjacencia)  
+if __name__ == "__main__":
+    dataFilme = pd.read_csv("ml-latest-small\\movies.csv") 
+    dataNotas = pd.read_csv("ml-latest-small\\ratings.csv")
+    geradorDeMatriz = geradorDeMatriz(dataNotas,dataFilme)
+    matrizDeAdjacencia = geradorDeMatriz.gerarMatrizDeAdjacencia()
